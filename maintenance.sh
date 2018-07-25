1d6aa81eee179270ee6951e15c592ecbc04fb657
Aqila F. Karim <32214317+AqilaKarim@users.noreply.github.com>
2018-07-25 17:36:45 +0700

    Update using group chat

--- maintenance.sh
+++ maintenance.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 mmcli=`which mmcli`
 nmcli=`which nmcli`
@@ -8,10 +8,16 @@ connected_modem=`$nmcli c s -a | grep gsm | wc -l`
 detected_modem=`mmcli -L | grep Modem | wc -l`
 
 redial(){
-	disconnect_modem=`cat /tmp/modem_check/check_list.txt | wc -l`
+	disconnect_modem=`cat /tmp/modem_check/result_list.txt | grep unknown | wc -l`
 	echo "Reconnecting modem ..."
 
-	#sleep 30
+	restart modem
+	for i in `seq 1 $disconnect_modem`; do
+		modem_id=`cat /tmp/modem_check/result_list.txt | grep "unknown" | awk {'print $1'} | sed -n "$i"p`
+		$mmcli -r -m $modem_id
+	done
+
+	sleep 60
 
 	#redial modem
 	for i in `seq 1 $disconnect_modem`; do
@@ -27,39 +33,40 @@ fi
 
 #remove temporary file
 mkdir -p /tmp/modem_check 
-rm -f /tmp/modem_check/{provider_list,gateway_list,check_list,result_list,message}.txt
+rm -f /tmp/modem_check/{id_list,provider_list,ifname_list,check_list,result_list,tmp_list,result_list,message}.txt
 
 #check from detected modem
 for _id in `$mmcli -L | awk '{print $1}' | sed 1,2d`; do
 	modem_id=`basename "$_id"`
 	provider=`$mmcli -m $modem_id | grep "operator name" | awk '{print $4$5}' | cut -d "'" -f2`
-	gateway_ip=`$nmcli c s ${provider,,} | grep IP4.GATEWAY | awk '{print $2}'`
+	ifname=`$mmcli -m $modem_id | grep primary | awk '{print $4}' | cut -d "'" -f2`
 	
+	echo $modem_id >> /tmp/modem_check/id_list.txt
 	echo ${provider,,} >> /tmp/modem_check/provider_list.txt
-	echo $gateway_ip >> /tmp/modem_check/gateway_list.txt
+	echo $ifname >> /tmp/modem_check/ifname_list.txt
 done
 
-paste -d' ' /tmp/modem_check/provider_list.txt /tmp/modem_check/gateway_list.txt >> /tmp/modem_check/result_list.txt
+paste -d' ' /tmp/modem_check/id_list.txt /tmp/modem_check/provider_list.txt >> /tmp/modem_check/tmp_list.txt
+paste -d' ' /tmp/modem_check/tmp_list.txt /tmp/modem_check/ifname_list.txt >> /tmp/modem_check/result_list.txt
 
 #detected vs connected
 if [ "$connected_modem" == "$detected_modem" ]; then
 	echo "all of $connected_modem modem(s) connected" >> /tmp/modem_check/message.txt 
-else
-	echo "$connected_modem of $detected_modem modem(s) disconnected" >> /tmp/modem_check/message.txt 
+	exit
+else 
+	echo "$(($detected_modem - $connected_modem)) of $detected_modem modem(s) disconnected" >> /tmp/modem_check/message.txt 
 	for i in `seq 1 $detected_modem`; do
-		if [[ -z `$cat /tmp/modem_check/result_list.txt | sed -n "$i"p | awk {'print $2'}` ]]; then
-			check=`$cat /tmp/modem_check/result_list.txt | sed -n "$i"p | awk {'print $1'}`
+		check=`$nmcli c s | grep gsm | awk {'print $1'} | sed -n "$i"p`
+		if [[ -z `$cat /tmp/modem_check/result_list.txt | awk {'print $2'} | grep "$check"` ]]; then
 			echo "$check disconnected" >> /tmp/modem_check/message.txt 
-			echo "$check" >> /tmp/modem_check/check_list.txt		
+			echo "$check" >> /tmp/modem_check/check_list.txt
 		fi
 	done
 	redial
 fi
 
-msg=`$cat /tmp/modem_check/message.txt`
+token="697263182:AAEljlmqD5wGKO1q6eSb6_Sn710gIOWey0s"
+msg=cat /tmp/modem_check/message.txt
+chatid="-272438846"
+curl -s -F chat_id=$chatid -F text="$msg" https://api.telegram.org/bot$token/sendMessage > /dev/null
 
-while IFS= read line
-do
-  token="697263182:AAEljlmqD5wGKO1q6eSb6_Sn710gIOWey0s"
-  curl -s -F chat_id=$line -F text="$msg" https://api.telegram.org/bot$token/sendMessage > /dev/null
-done < /home/hilmi/bot/chatid.txt
